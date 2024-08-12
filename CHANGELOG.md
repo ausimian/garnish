75e4fc240091c54a1ac37ee232304143195db10f
Nick Gunn <nick@ausimian.net>
2024-08-12 22:34:36 +1000

    docs: add changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+## [0.1.0] - 2024-08-12
+
+### Known issues
+
+- The logical rendering process is relatively expensive. For views that change
+  substantially and on a frequent basis, it is _not_ web-scale.
+- Only xterm-256color is supported in this release.
--- mix.exs
+++ mix.exs
@@ -2,15 +2,18 @@ defmodule Garnish.MixProject do
   use Mix.Project
 
   @version "0.1.0"
+  @scm_url "https://github.com/ausimian/garnish"
 
   def project do
     [
       app: :garnish,
       version: @version,
       elixir: "~> 1.15",
+      source_url: @scm_url,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      docs: docs()
+      docs: docs(),
+      package: package()
     ]
   end
 
@@ -30,12 +33,23 @@ defmodule Garnish.MixProject do
     ]
   end
 
+  defp package do
+    [
+      maintainers: ["ausimian"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @scm_url},
+      files:
+        ~w(lib CHANGELOG.md LICENSE.md mix.exs README.md .formatter.exs)
+    ]
+  end
+
   defp docs do
     [
       source_ref: "v#{@version}",
       main: "readme",
       extras: [
-        "README.md"
+        "README.md",
+        "CHANGELOG.md"
       ]
     ]
   end
